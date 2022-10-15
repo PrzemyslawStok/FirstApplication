3410fc5dba731ef543e03515999b079aa9174d8f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-15 09:25:03 +0200

    cd ..

--- app/src/main/java/com/com/firstapplication/MainActivity.kt
+++ app/src/main/java/com/com/firstapplication/MainActivity.kt
@@ -9,13 +9,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tag = "MainActivity"
+
         val a = 10.0f
         println("Wprowadzono liczbę: ${a}")
-        Log.i("MainActivity","Wprowadzono liczbę: ${a}")
+        Log.i("MainActivity", "Wprowadzono liczbę: ${a}")
 
-        for( i in 1..10){
-            println("$i")
+        for (i in 1..10) {
+            Log.i(tag,"$i")
+        }
+
+        val x = 1.0
+        val y = 2.0
+
+        Log.i(tag,"$x+$y=${add(x, y)}")
+
+        val A = listOf(1, 2, 3, 4, 5)
+
+        for (a0 in A) {
+            Log.i(tag,"$a0")
         }
 
     }
+
+    fun add(a: Double, b: Double): Double {
+        return a + b
+    }
+
 }
