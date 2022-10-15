8411b5f41ee289f05b72c13e9dae1dd8bff76fd9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-15 09:40:31 +0200

    cd ..

--- app/src/main/java/com/com/firstapplication/MainActivity.kt
+++ app/src/main/java/com/com/firstapplication/MainActivity.kt
@@ -16,18 +16,26 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity", "Wprowadzono liczbę: ${a}")
 
         for (i in 1..10) {
-            Log.i(tag,"$i")
+            Log.i(tag, "$i")
         }
 
         val x = 1.0
         val y = 2.0
 
-        Log.i(tag,"$x+$y=${add(x, y)}")
+        Log.i(tag, "$x+$y=${add(x, y)}")
 
-        val A = listOf(1, 2, 3, 4, 5)
+        val A = mutableListOf(1, 2, 3, 4, 5)
+        A.add(20)
+
 
         for (a0 in A) {
-            Log.i(tag,"$a0")
+            Log.i(tag, "$a0")
+        }
+
+        val B = IntArray(10) { it + 1 }
+
+        for (b0 in B) {
+            Log.i(tag, "$b0")
         }
 
     }
