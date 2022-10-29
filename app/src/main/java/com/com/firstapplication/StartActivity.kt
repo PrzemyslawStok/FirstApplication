51e7e180b80d8722958b3e0e305e5bd128db7b52
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-29 09:40:37 +0200

    cd ..

--- app/src/main/java/com/com/firstapplication/StartActivity.kt
+++ app/src/main/java/com/com/firstapplication/StartActivity.kt
@@ -1,11 +1,19 @@
 package com.com.firstapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
+    var buttonGame1: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        buttonGame1 = findViewById(R.id.buttonGame1)
+        buttonGame1?.setOnClickListener {
+
+        }
+
     }
 }
