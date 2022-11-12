8694ff7df83c353e392cbd343a13342dbffd579d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-12 09:41:00 +0100

    cd ..

--- app/src/main/java/com/com/firstapplication/Game2Activity.kt
+++ app/src/main/java/com/com/firstapplication/Game2Activity.kt
@@ -7,14 +7,41 @@ import com.com.firstapplication.databinding.ActivityGame2Binding
 class Game2Activity : AppCompatActivity() {
     lateinit var binding: ActivityGame2Binding
 
+    val leftInit = -5
+    val rightInit = -7
+    val downInit = 11
+    val upInit = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGame2Binding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
+        initLayout()
+
+        resetGame()
+
 
         //setContentView(R.layout.activity_game2)
+
+    }
+
+    fun initLayout() {
+        binding.viewRight.text = initText(rightInit)
+        binding.viewLeft.text = initText(leftInit)
+        binding.viewDown.text = initText(downInit)
+        binding.viewUp.text = initText(upInit)
+    }
+
+    fun initText(value: Int): String {
+        if (value > 0)
+            return "+$value"
+        else
+            return "$value"
+    }
+
+    fun resetGame() {
 
     }
 }
