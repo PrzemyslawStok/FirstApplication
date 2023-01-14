b0d8a5ae770e98a7023ec86fec4eae608dcd8550
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-14 09:05:16 +0100

    cd ..

--- app/src/main/java/com/com/firstapplication/Game3Activity.kt
+++ app/src/main/java/com/com/firstapplication/Game3Activity.kt
@@ -1,11 +1,25 @@
 package com.com.firstapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.com.firstapplication.databinding.ActivityGame3Binding
 
 class Game3Activity : AppCompatActivity() {
+    lateinit var binding: ActivityGame3Binding
+    lateinit var testButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game3)
+
+        binding = ActivityGame3Binding.inflate(layoutInflater)
+        testButton = binding.testButton
+
+        setContentView(binding.root)
+
+        testButton.setOnClickListener {
+            val gameAgl = Game3Algorithm()
+            gameAgl.printGameArray()
+        }
     }
 }
---
+++ app/src/main/java/com/com/firstapplication/Game3Algorithm.kt
@@ -0,0 +1,15 @@
+package com.com.firstapplication
+
+import android.util.Log
+
+class Game3Algorithm {
+    var levelTable = arrayOf(
+        intArrayOf(0, 0, 1),
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0)
+    )
+
+    fun printGameArray() {
+        Log.i("GameArray", "Game array")
+    }
+}
