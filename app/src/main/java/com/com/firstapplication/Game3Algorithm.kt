8dba8b046ac8e23ad0cd34245d3654388943ddab
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-14 09:32:03 +0100

    cd ..

--- app/src/main/java/com/com/firstapplication/Game3Algorithm.kt
+++ app/src/main/java/com/com/firstapplication/Game3Algorithm.kt
@@ -4,6 +4,8 @@ import android.util.Log
 
 class Game3Algorithm {
     val tag = "GameArray"
+    val size = 3
+
     var levelTable = arrayOf(
         intArrayOf(0, 0, 1),
         intArrayOf(0, 0, 0),
@@ -12,7 +14,7 @@ class Game3Algorithm {
 
     fun arrayRowToString(row: Int): String {
         var rowText = ""
-        for (j in 0..2) {
+        for (j in 0..size) {
             rowText += "${levelTable[row][j]} "
         }
         return rowText
@@ -20,8 +22,12 @@ class Game3Algorithm {
 
     fun printGameArray() {
         Log.i(tag, "Game array:")
-        for (row in 0..2) {
+        for (row in 0..size) {
             Log.i(tag, arrayRowToString(row))
         }
+    }
+
+    fun invertRegion(row: Int, col: Int){
+
     }
 }
