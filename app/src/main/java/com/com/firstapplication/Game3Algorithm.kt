9ee863e08db78400d55dc5bae94b1f69d6783ea3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-14 09:27:05 +0100

    cd ..

--- app/src/main/java/com/com/firstapplication/Game3Algorithm.kt
+++ app/src/main/java/com/com/firstapplication/Game3Algorithm.kt
@@ -3,13 +3,26 @@ package com.com.firstapplication
 import android.util.Log
 
 class Game3Algorithm {
+    val tag = "GameArray"
     var levelTable = arrayOf(
         intArrayOf(0, 0, 1),
         intArrayOf(0, 0, 0),
         intArrayOf(0, 0, 0)
     )
 
+    fun arrayRowToString(row: Int): String {
+        var rowText = ""
+        for (j in 0..2) {
+            rowText += "${levelTable[row][j]} "
+        }
+        return rowText
+    }
+
     fun printGameArray() {
-        Log.i("GameArray", "Game array")
+        Log.i(tag, "Game array")
+        for (row in 0..2) {
+            Log.i(tag, arrayRowToString(row))
+        }
+
     }
 }
